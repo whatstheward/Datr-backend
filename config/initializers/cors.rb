093756091b8a71a68177d2bf03265d93974e1d70
github username <github email address>
2019-06-20 09:58:48 -0400

    fix cors origin

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-        origins 'https://datr-atl.herokuapp.com/'
+        origins 'https://datr-atl.herokuapp.com'
         resource '*', headers: :any, methods: [:get, :post, :delete, :put, :patch, :options]
     end
 end
