304d8c991098165e2b67cd3b06626300c13f4268
Maddie <whatstheward@gmail.com>
2019-11-05 14:09:17 -0500

    add rating to userdate

--- db/migrate/20191105182525_add_rating_to_user_date.rb
+++ db/migrate/20191105182525_add_rating_to_user_date.rb
@@ -1,5 +1,8 @@
 class AddRatingToUserDate < ActiveRecord::Migration[5.2]
-  def change
+  def up
     add_column :user_dates, :rating, :integer, :default => 0
+  end
+  def down
+    remove_column :user_dates, :rating
   end
 end
